990382b2a9e29153242039c06314617266b1e07b
Jason Barden <jason.barden@outlook.com>
2026-07-17 13:50:37 +0100

    maint: add case-insensitivity to multiple Name fields (#73)

---
+++ AStar.Dev.Infrastructure.AppDb/Migrations/20260717111216_AddCaseInsensitivityToSomeColumns.cs
@@ -0,0 +1,111 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AStar.Dev.Infrastructure.AppDb.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCaseInsensitivityToSomeColumns : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_FileClassificationCategories_Name",
+                table: "FileClassificationCategories");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Value",
+                table: "TagToIgnore",
+                type: "TEXT",
+                maxLength: 300,
+                nullable: false,
+                collation: "NOCASE",
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldMaxLength: 300);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "SearchCategories",
+                type: "TEXT",
+                maxLength: 256,
+                nullable: false,
+                collation: "NOCASE",
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldMaxLength: 256);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Value",
+                table: "ModelToIgnore",
+                type: "TEXT",
+                maxLength: 300,
+                nullable: false,
+                collation: "NOCASE",
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldMaxLength: 300);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "FileClassificationCategories",
+                type: "TEXT",
+                nullable: false,
+                collation: "NOCASE",
+                oldClrType: typeof(string),
+                oldType: "TEXT");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Value",
+                table: "TagToIgnore",
+                type: "TEXT",
+                maxLength: 300,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldMaxLength: 300,
+                oldCollation: "NOCASE");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "SearchCategories",
+                type: "TEXT",
+                maxLength: 256,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldMaxLength: 256,
+                oldCollation: "NOCASE");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Value",
+                table: "ModelToIgnore",
+                type: "TEXT",
+                maxLength: 300,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldMaxLength: 300,
+                oldCollation: "NOCASE");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "FileClassificationCategories",
+                type: "TEXT",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldCollation: "NOCASE");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_FileClassificationCategories_Name",
+                table: "FileClassificationCategories",
+                column: "Name");
+        }
+    }
+}
