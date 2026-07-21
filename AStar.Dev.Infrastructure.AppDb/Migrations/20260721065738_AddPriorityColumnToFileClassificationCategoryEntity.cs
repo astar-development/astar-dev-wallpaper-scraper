0a79cb8d9f333764ec891bea6b1fd6b3084ad7e8
Jason Barden <jason.barden@outlook.com>
2026-07-21 16:24:44 +0100

    Add a Priority for ordering the FileClassifications

---
+++ AStar.Dev.Infrastructure.AppDb/Migrations/20260721065738_AddPriorityColumnToFileClassificationCategoryEntity.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AStar.Dev.Infrastructure.AppDb.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPriorityColumnToFileClassificationCategoryEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Priority",
+                table: "FileClassificationCategories",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Priority",
+                table: "FileClassificationCategories");
+        }
+    }
+}
